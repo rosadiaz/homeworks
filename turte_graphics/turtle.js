bc61c7492d4ed69639d075a7e6df9df7e4ac034b
Rosa Code Core <RosaCC@Rosas-MBP.hitronhub.home>
2018-09-22 09:49:10 -0700

    Turtle can turn right, woohoo

--- turte_graphics/turtle.js
+++ turte_graphics/turtle.js
@@ -1,19 +1,67 @@
-
 class Turtle {
   constructor (x,y) {
     this.x = x;
     this.y = y;
-    this.position = [x,y]
+    this.position = [x,y];
+    this.path = [[x,y]];
+    this.direction = "x";
   }
   
   forward(steps) {
-    this.position = [(this.x + steps), this.y];
+    if (this.direction === "x") {
+      this.x += steps;
+      this.position = [this.x, this.y];
+      this.path.push(this.position);
+    } else if (this.direction === "-y") {
+      this.y -= steps;
+      this.position = [this.x, this.y];
+      this.path.push(this.position);
+    } else if (this.direction === "-x") {
+      this.x -= steps;
+      this.position = [this.x, this.y];
+      this.path.push(this.position);
+    } else if (this.direction === "y") {
+      this.y += steps;
+      this.position = [this.x, this.y];
+      this.path.push(this.position);
+    }
     return this.position
+  }
+
+  right() {
+    if (this.direction === "x" ){
+      this.direction = "-y";
+    } else if (this.direction === "-y") {
+      this.direction = "-x";
+    } else if (this.direction === "-x") {
+      this.direction = "y";
+    }
+    return this.direction;
   }
 }
 
-console.log('🐢 🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾')
-const tommy = new Turtle(3,3);
-console.log(`Tommy the turtle is at ${tommy.position}`);
+console.log('🐢 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾')
+const tommy = new Turtle(0,0);
+console.log(`Tommy the turtle started at ${tommy.position}`);
+console.log(`Tommy move forward 3`)
 tommy.forward(3);
 console.log(`He is now at ${tommy.position}`);
+console.log(`Tommy turn right!`);
+tommy.right();
+console.log(`He will now advance in ${tommy.direction}`);
+console.log(`Tommy move forward 2`)
+tommy.forward(2);
+console.log(`He is now at ${tommy.position}`);
+console.log(`Tommy turn right!`);
+tommy.right();
+console.log(`He will now advance in ${tommy.direction}`);
+console.log(`Tommy move forward 3`)
+tommy.forward(3);
+console.log(`He is now at ${tommy.position}`);
+console.log(`Tommy turn right!`);
+tommy.right();
+console.log(`He will now advance in ${tommy.direction}`);
+console.log(`Tommy move forward 2`)
+tommy.forward(2);
+console.log(`He is now at ${tommy.position}`);
+console.log(tommy.path);

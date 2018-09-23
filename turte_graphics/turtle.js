678102feb7aaa39c7c42ef4f8eaec28212185ed9
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-09-23 09:58:26 -0700

    Defines canvas size for drawing

--- turte_graphics/turtle.js
+++ turte_graphics/turtle.js
@@ -45,45 +45,62 @@ class Turtle {
   }
 
   print(path) {
-    for (let position of path) {
-
+    const full = '\u2588';
+    const empty = " ";
+    let maxY = -Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let minX = Infinity;
+  
+    // find the highest coordinate on Y axis to begin drawing
+    for (let position of this.path) {
+      if (position[1] > maxY) {
+        maxY = position[1];
+      }
     }
+    //find the lowest coordinate on Y axis to end drawing
+    for (let position of this.path) {
+      if (position[1] < minY) {
+        minY = position[1];
+      }
+    }
+    //find the width of the drawing with maxX and minX
+    for (let position of this.path) {
+      if (position[0] > maxX) {
+        maxX = position[0];
+      }
+    }
+    for (let position of this.path) {
+      if (position[0] < minX) {
+        minX = position[0];
+      }
+    }
+    console.log(`Drawing top-left: (${maxY}, ${minX}), bottom-right: (${maxX}, ${minY})`)
+
+    //draw each line
+    // let drawing = "";
+    // for (let x = minX; x <= maxX; x++) {
+    //   for (let y = minY; y <= maxY; y++) {
+    //     console.log(x,y);
+    //     function exists (coordinate, x, y) {
+    //       return coordinate === [x,y];
+    //     }
+
+    //     if (this.path.each(exists)) {
+    //       drawing += `${full}`;
+    //     } else {
+    //       drawing += `${empty}`;
+    //     }
+    //   }
+    //   console.log(drawing);
+    // }
   }
 }
 
+console.log(' ')
 console.log('🐢 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾')
-// const tommy = new Turtle(0,0);
-// console.log(`Tommy the turtle started at ${tommy.position}`);
-// console.log(`Tommy move forward 3`)
-// tommy.forward(3);
-// console.log(`He is now at ${tommy.position}`);
-// console.log(`Tommy turn right!`);
-// tommy.right();
-// console.log(`He will now advance in ${tommy.direction}`);
-// console.log(`Tommy move forward 2`)
-// tommy.forward(2);
-// console.log(`He is now at ${tommy.position}`);
-// console.log(`Tommy turn LEFT!`);
-// tommy.left();
-// console.log(`He will now advance in ${tommy.direction}`);
-// console.log(`Tommy move forward 3`)
-// tommy.forward(3);
-// console.log(`He is now at ${tommy.position}`);
-// console.log(`Tommy turn right!`);
-// tommy.right();
-// console.log(`He will now advance in ${tommy.direction}`);
-// console.log(`Tommy move forward 2`)
-// tommy.forward(2);
-// console.log(`He is now at ${tommy.position}`);
-// console.log(tommy.path);
 
 const tommy = new Turtle(0, 0).forward(3).right().forward(2).left().forward(3).right().forward(2);
-// console.log(`[ [ 0, 0 ], [ 3, 0 ], [ 3, -2 ], [ 6, -2 ], [ 6, -4 ] ]`);
 console.log('Tommy starts at (0,0), moves forward 3, turns right, moves forward 2, turns lef, moves forward 3, turns right, moves forward 2. He should finish at (6,-4)')
-console.log(tommy);
-
-// //This turtle begins at position (0, 4), then moves forward 3 steps to (3, 4)
-// //then turns right (facing north), then moves 3 steps to (3, 1).
-// console.log('Start at -> 0,4, move forward 3 -> (3,4) turn right, move 3 -> (3,1)')
-// console.log(new Turtle(0, 4).forward(3).right().forward(3));
-
+console.log(tommy.path);
+console.log(tommy.print(tommy.path));

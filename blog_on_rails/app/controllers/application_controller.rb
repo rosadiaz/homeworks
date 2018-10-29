7fbaff342152b93f200c4c63c694123cee84b1df
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-10-28 22:17:35 -0700

    Renames user to current_user in app controller
    Naming collisionwith sessions controller

--- blog_on_rails/app/controllers/application_controller.rb
+++ blog_on_rails/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   private
   def current_user
-    @user ||= User.find_by_id(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id])
   end
   helper_method :current_user #only if views will use this method
 

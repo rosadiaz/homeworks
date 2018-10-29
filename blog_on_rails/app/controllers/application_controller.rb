c91199f3aca0daf6f53b3337e20a19d13f50e07b
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-10-28 21:41:16 -0700

    Adds authentications to posts

--- blog_on_rails/app/controllers/application_controller.rb
+++ blog_on_rails/app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user #only if views will use this method
 
+  def authenticate_user!
+    unless current_user
+      flash[:alert] = "Please sign in"
+      redirect_to root_path
+    end
+  end
+
 end

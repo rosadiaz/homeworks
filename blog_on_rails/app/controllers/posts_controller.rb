0665ec40be53079222b4a8e77782434f618f591b
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-10-28 22:26:59 -0700

    Associates current_user to new posts and comments

--- blog_on_rails/app/controllers/posts_controller.rb
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
   end
   def create
     @post = Post.new post_params
+    @post.user = current_user
     if @post.save
       redirect_to post_path(@post.id)
     else

931b02d7bb5e3bf2f4045b6a28ff6bd183e9704a
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-10-28 21:38:29 -0700

    Moved current_user helper to application controller

--- blog_on_rails/app/controllers/application_controller.rb
+++ blog_on_rails/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  private
+  def current_user
+    @user ||= User.find_by_id(session[:user_id])
+  end
+  helper_method :current_user #only if views will use this method
 
 end
